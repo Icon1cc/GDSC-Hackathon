ce9c139390ce96c7e4c0cdcb438e3b22b9da0cbf
FISHCAT8 <82520225+Doughet@users.noreply.github.com>
2024-04-21 03:38:45 +0200

    all parsers + QCMS

--- querypreprocessor.py
+++ querypreprocessor.py
@@ -82,11 +82,11 @@ def querry_QAs(query):
     detailed_query = (
         f"I will write a description of the way I want you to answer to the following question."
         f"The question that the user wants to learn about (studying about its context) will be : {query}"
-        f"Identify the three to five most important ideas required before being able to answer this question. These idea should be such that if the user understands them all, he can answer the question by himself, not directly give him the answer."
+        f"Identify the three most important ideas required before being able to answer this question. These idea should be such that if the user understands them all, he can answer the question by himself, not directly give him the answer."
         f"The template of the answer would be:"
-        f"- Start your answer with word \"START\" and end with \"END\"."
-        f"- For each one of the important ideas selected write three questions about aspects that the user should know after looking up for that idea + well detailed answers to those questions"
-        f"- Express them as \"STARTLINE\" + the question + \"ANSLINE\" + the answer."
+        f"- Start your answer with word \" START \" and end with \" END \"."
+        f"- For each one of the important ideas selected write a difficult question about an aspect of it, that the user should know after looking up for that idea + three answers to that question, first one being true and second two being completely false."
+        f"- Express them on one line as \" STARTLINE \" + the question + \" ANSFI \" + the correct answer + \" ANSSE \" + first completely wrong answer + \" ANSTHI \" + second completely wrong answer. All this without line break."
     )
     return detailed_query
 
@@ -94,12 +94,11 @@ def querry_QAs2(query, context = ""):
     detailed_query = (
         f"I will write a description of the way I want you to answer to the following question."
         f"Based mainly on this document: {context}"
-        f"The question that the user wants to learn about (studying about its context) will be : {query}"
-        f"Identify the three to five most important ideas required before being able to answer this question. These idea should be such that if the user understands them all, he can answer the question by himself, not directly give him the answer."
+        f"Identify the three most important ideas required before being able to answer this question. These idea should be such that if the user understands them all, he can answer the question by himself, not directly give him the answer."
         f"The template of the answer would be:"
-        f"- Start your answer with word \"START\" and end with \"END\"."
-        f"- For each one of the important ideas selected write three questions about aspects that the user should know after looking up for that idea + well detailed answers to those questions"
-        f"- Express them as \"STARTLINE\" + the question + \"ANSLINE\" + the answer."
+        f"- Start your answer with word \" START \" and end with \" END \"."
+        f"- For each one of the important ideas selected write a difficult question about an aspect of it, that the user should know after looking up for that idea + three answers to that question, first one being true and second two being completely false."
+        f"- Express them on one line as \" STARTLINE \" + the question + \" ANSFI \" + the correct answer + \" ANSSE \" + first completely wrong answer + \" ANSTHI \" + second completely wrong answer. All this without line break."
     )
     return detailed_query
 
@@ -110,9 +109,10 @@ def query_summary_sheet(query):
         f"Identify the most important idea that this question tackles."
         f"The template of the answer would be:"
         f"- Start your answer with word \"START\"."
-        f"- First paragraph \"IDEA\" + One paragraph will be a short title of the idea chosen + \": \" +  a detailed description of the idea you chose."
-        f"- The next paragraphs will be for two or three  key aspects concerning this idea: [\"PAR_TITLE\" + a short title of the idea chosen + \"EXP\" +  explanations in many details of that aspect] ."
-        f"- Next paragraph will start by \"SUMMARY\" + a summary about the information presented"
+        f"- First paragraph expressed without a line break as \"IDEPAR\" + a short title of the most important idea that this question tackles + \"IDEDESC \" +  a detailed description of the idea you chose."
+        f"- The next paragraphs will be for two or three key aspects concerning this idea"
+        f"They will be expressed without line break as \"PARTIT\" + a short title of the idea chosen + \"PARTEXP\" +  explanations in many details of that aspect] ."
+        f"- Next paragraph will start by \"SUMALL\" + a summary about the information presented"
         f"It should end with the word \"END\""
     )
     return detailed_query
@@ -125,9 +125,10 @@ def query_summary_sheet2(query, context = ""):
         f"Identify the most important idea that this question tackles."
         f"The template of the answer would be:"
         f"- Start your answer with word \"START\"."
-        f"- First paragraph \"IDEA\" + One paragraph will be a short title of the idea chosen + \": \" +  a detailed description of the idea you chose."
-        f"- The next paragraphs will be for two or three  key aspects concerning this idea: [\"PAR_TITLE\" + a short title of the idea chosen + \"EXP\" +  explanations in many details of that aspect] ."
-        f"- Next paragraph will start by \"SUMMARY\" + a summary about the information presented"
+        f"- First paragraph expressed without a line break as \"IDEPAR\" + a short title of the most important idea that this question tackles + \"IDEDESC \" +  a detailed description of the idea you chose."
+        f"- The next paragraphs will be for two or three key aspects concerning this idea"
+        f"They will be expressed without line break as \"PARTIT\" + a short title of the idea chosen + \"PARTEXP\" +  explanations in many details of that aspect] ."
+        f"- Next paragraph will start by \"SUMALL\" + a summary about the information presented"
         f"It should end with the word \"END\""
     )
     return detailed_query
@@ -138,8 +139,6 @@ def parse_bullet_answer(answer):
     list_lines = []
 
     titles = []
-    descrs = []
-
 
     title_sep = "TITIDE "
     desc_sep = " DESCIDE "
@@ -167,5 +166,114 @@ def parse_bullet_answer(answer):
         titles.append(tempDesc)
 
     print(titles)
+
+    return titles
+
+
+def parse_QA_answer(answer):
+    list_titles = []
+    list_descs1 = []
+    list_descs2 = []
+    list_descs3 = []
+    list_lines = []
+
+    titles = []
+
+    title_sep = "STARTLINE "
+    desc_sep1 = " ANSFI "
+    desc_sep2 = " ANSSE "
+    desc_sep3 = " ANSTHI "
+
+    start = 0  # initial search position
+    while True:
+        start = answer.find(title_sep, start)
+        end1 = answer.find(desc_sep1, start)
+        end2 = answer.find(desc_sep2, end1)
+        end3 = answer.find(desc_sep3, end1)
+        line = answer.find("\n", end3)
+        if start == -1:  # no more occurrences found
+            break
+
+        list_titles.append(start)
+        list_descs1.append(end1)
+        list_descs2.append(end2)
+        list_descs3.append(end3)
+        list_lines.append(line)
+        start += 1
+        end1 += 1
+
+    for i in range(0, len(list_titles)):
+
+        tempTitle = answer[list_titles[i] + len(title_sep): list_descs1[i]]
+        tempDesc1 = answer[list_descs1[i] + len(desc_sep1): list_descs2[i]]
+        tempDesc2 = answer[list_descs2[i] + len(desc_sep2): list_descs3[i]]
+        tempDesc3 = answer[list_descs3[i] + len(desc_sep3): list_lines[i]]
+        titles.append(tempTitle)
+        titles.append(tempDesc1)
+        titles.append(tempDesc2)
+        titles.append(tempDesc3)
+
+    print(titles)
+
+    return titles
+
+def parse_summary_answer(answer):
+
+    #LES OUTPUTS CEST TITLEIDEA, DESCIDEA, TITLES, SUMALL
+
+
+    titleIdea = ""
+    descIdea = ""
+
+
+    startIntro = "IDEPAR "
+    titleIntro = " IDEDESC "
+
+    start = answer.find(startIntro, 0)
+    end = answer.find(titleIntro, 0)
+    descend = answer.find("\n", 0)
+
+    titleIdea = answer[start + len(startIntro): end]
+    descIdea = answer[end + len(titleIntro): descend]
+
+    list_titles = []
+    list_descs = []
+    list_lines = []
+
+    titles = []
+
+    title_sep = "PARTIT "
+    desc_sep = " PARTEXP "
+
+    start = 0  # initial search position
+    end = 0
+    while True:
+        start = answer.find(title_sep, start)
+        end = answer.find(desc_sep, end)
+        line = answer.find("\n", end)
+        if start == -1:  # no more occurrences found
+            break
+
+        list_titles.append(start)
+        list_descs.append(end)
+        list_lines.append(line)
+        start += 1
+        end += 1
+
+    for i in range(0, len(list_titles)):
+
+        tempTitle = answer[list_titles[i] + len(title_sep): list_descs[i]]
+        tempDesc = answer[list_descs[i] + len(desc_sep): list_lines[i]]
+        titles.append(tempTitle)
+        titles.append(tempDesc)
+
+    sepSumall = "SUMALL "
+    indexSumall = answer.find(sepSumall)
+    sumall = answer[indexSumall + len(sepSumall) : answer.find("\n",indexSumall)]
+
+    print(titleIdea)
+    print(descIdea)
+    print(titles)
+    print(sumall)
 
     return titles
